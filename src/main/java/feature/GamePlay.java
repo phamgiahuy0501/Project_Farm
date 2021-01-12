a1146221452717a8f1ae7a71bcd68394da7f3492
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2021-01-12 23:26:40 +0700

    Last update

--- src/main/java/feature/GamePlay.java
+++ src/main/java/feature/GamePlay.java
@@ -11,7 +11,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
